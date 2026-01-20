42cd714de99f904cf5a6605c6c16f69648b67688
abengl <126703861+abengl@users.noreply.github.com>
2026-01-20 15:17:22 -0500

    :building_construction: enhance: update transaction model and initialization logic, add TransactionStatus enum

--- src/main/java/com/alessandragodoy/customerms/configuration/DataLoader.java
+++ src/main/java/com/alessandragodoy/customerms/configuration/DataLoader.java
@@ -25,29 +25,22 @@ public class DataLoader implements CommandLineRunner {
 	public void run(String... args) {
 		if (customerRepository.count() == 0) {
 			LOGGER.info("No customers found, creating initial customers...");
-			List<Customer> initialCustomers = List.of(
-					Customer.builder().firstName("Julia").lastName("Mendez")
-							.documentNumber("11111111")
-							.email("jmendez@mail.com").phoneNumber("963852741")
-							.address("Lima, Perú").build(),
-					Customer.builder().firstName("Alicia").lastName("Ramirez")
-							.documentNumber("22222222")
-							.email("aramirez@mail.com").phoneNumber("741852963")
-							.address("Lima, Perú").build(),
-					Customer.builder().firstName("Jose").lastName("Melendez")
-							.documentNumber("33333333")
-							.email("jmelendez@mail.com").phoneNumber("863214569")
-							.address("Lima, Perú").build(),
-					Customer.builder().firstName("Carla").lastName("Gomez")
-							.documentNumber("44444444")
-							.email("cgomez@mail.com").phoneNumber("963963852").address("Lima, " +
-									"Perú")
-							.build(),
-					Customer.builder().firstName("Juan").lastName("Perez")
-							.documentNumber("55555555")
-							.email("jperez@mail.com").phoneNumber("741874521").address("Lima, " +
-									"Perú").build()
-			);
+			List<Customer> initialCustomers =
+					List.of(Customer.builder().firstName("Julia").lastName("Mendez")
+									.documentNumber("11111111").email("jmendez@mail.com")
+									.phoneNumber("963852741").address("Lima, Perú").build(),
+							Customer.builder().firstName("Alicia").lastName("Ramirez")
+									.documentNumber("22222222").email("aramirez@mail.com")
+									.phoneNumber("741852963").address("Lima, Perú").build(),
+							Customer.builder().firstName("Jose").lastName("Melendez")
+									.documentNumber("33333333").email("jmelendez@mail.com")
+									.phoneNumber("863214569").address("Lima, Perú").build(),
+							Customer.builder().firstName("Carla").lastName("Gomez")
+									.documentNumber("44444444").email("cgomez@mail.com")
+									.phoneNumber("963963852").address("Lima, " + "Perú").build(),
+							Customer.builder().firstName("Juan").lastName("Perez")
+									.documentNumber("55555555").email("jperez@mail.com")
+									.phoneNumber("741874521").address("Lima, " + "Perú").build());
 
 			customerRepository.saveAll(initialCustomers);
 			LOGGER.info("Initial customers added to the database.");
